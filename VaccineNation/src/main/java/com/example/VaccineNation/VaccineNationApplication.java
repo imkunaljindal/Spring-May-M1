78fde23e00cf57dc45dcc82692f90d101d02e956
kunaljindal <kunaljindal1254@gmail.com>
2024-05-24 23:18:58 +0530

    Add vaccination booking system project structure

---
+++ VaccineNation/src/main/java/com/example/VaccineNation/VaccineNationApplication.java
@@ -0,0 +1,11 @@
+package com.example.VaccineNation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineNationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineNationApplication.class, args);
+	}
+}
---
+++ VaccineNation/src/test/java/com/example/VaccineNation/VaccineNationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VaccineNation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineNationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
